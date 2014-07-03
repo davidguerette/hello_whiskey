91b89cd1c039c6cbafe6d6b1778ee8cbf8dda612
David Guerette <david.guerette@gmail.com>
2014-07-03 17:52:46 -0400

    Passes test to view index of drinks.

---
+++ spec/support/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :recipe do
+      name 'Manhattan'
+      directions 'Stir with ice and strain into chilled cocktail glass. Garnish with cherry.'
+  end
+end
+
+# ingredients ['2 oz rye or bourbon whiskey', '0.5 oz sweet vermouth', '1 dash Angustora bitters', '1 maraschino cherry']
--- spec/user_can_view_list_of_recipes_spec.rb
+++ spec/user_can_view_list_of_recipes_spec.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+
+feature 'user can view a list of cocktail recipes' do
+
+  scenario 'user can successfully view a list of all cocktail recipes' do
+    recipe = FactoryGirl.create(:recipe)
+binding.pry
+    visit root_path
+    click_on 'All Drinks'
+
+    expect(page).to have_content('All Drinks')
+    expect(page).to have_content(recipe.name)
+  end
+end
