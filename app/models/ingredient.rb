0db97c96afdd5b318e6524898d0b0dc74980c010
David Guerette <david.guerette@gmail.com>
2014-07-15 11:27:06 -0400

    Substitute ingredient associations functioning and added properly from seeder CSV.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,8 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :component
+  has_many :substitutions
+  has_many :substitutes, through: :substitutions, foreign_key: 'substitute_id', class_name: 'Component'
 
   validates :component, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-CSV.foreach("../holy_grail/db/data/hello_whiskey_seeder.csv", headers: true, header_converters: :symbol) do |row|
+CSV.foreach("../holy_grail/db/data/hello_whiskey_test_seeder.csv", headers: true, header_converters: :symbol) do |row|
   row = row.to_hash
 
   recipe = Recipe.find_or_create_by({name: row[:recipe_name], directions: row[:recipe_directions]})
   category = Category.find_or_create_by({name: row[:category_name]})
   component = Component.find_or_create_by({name: row[:component_name], category_id: category.id})
-  Ingredient.find_or_create_by({quantity: row[:ingredient_quantity], unit: row[:ingredient_unit],
+  ingredient = Ingredient.find_or_create_by({quantity: row[:ingredient_quantity], unit: row[:ingredient_unit],
     recipe_id: recipe.id, component_id: component.id})
+
+  if row[:substitution]
+    substitute_ingredient = Component.find_or_create_by({name: row[:substitution], category_id: category.id})
+    Substitution.find_or_create_by(ingredient_id: ingredient.id, substitute_id: substitute_ingredient.id)
+  end
 end
+
+binding.pry
