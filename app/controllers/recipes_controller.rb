9a714b75a86c292f702295394e9e1a8e8ccc4115
David Guerette <david.guerette@gmail.com>
2014-07-20 15:20:40 -0400

    Restructed welcome page steps

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,14 +5,14 @@ class RecipesController < ApplicationController
       component_ids = params[:search][:component_ids].reject(&:blank?).map(&:to_i)
       components = Component.find(component_ids)
       recipes_search = RecipeSearch.new(components)
-      @recipes = recipes_search.search
+      @recipes = recipes_search.search.order(name: :asc)
 
       if @recipes.empty?
         flash.now[:notice] = "Sorry, but fine drinks begin with fine ingredients. Please enter more ingredients and try again!"
         render :search
       end
     else
-      @recipes = Recipe.all
+      @recipes = Recipe.all.order(name: :asc)
     end
   end
 
