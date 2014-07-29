b98220b1e7f54939fbf392ce99853ad2b26d4a79
David Guerette <david.guerette@gmail.com>
2014-07-29 09:20:09 -0400

    Units display as integers and fractions

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -8,6 +8,36 @@ class Ingredient < ActiveRecord::Base
 
   validates :component, presence: true
 
+  # quantity: if quantity % (1) == 0
+  #             quantity.to_i
+  #           else
+  #             quantity.to_r
+  #           end
+
+
+  def display_quantity
+    if self.quantity % 1 == 0
+      quantity.to_i
+    else
+      if self.quantity > 1
+        remainder = (quantity % 1)
+        integer = (quantity - remainder).to_i
+        "#{integer} #{remainder.to_r}"
+      else
+        quantity.to_r
+      end
+    end
+end
+
+
+  #unit
+
+
+  #citrus:
+
+
+
+
   def citrus_liquid_measure?
     if self.component.category.name == 'citrus' && LIQUID_MEASURE_UNITS.include?(self.unit)
       true
